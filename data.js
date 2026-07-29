baa693629f8498b4eeb2e10dab687e81c00e124b
ejscaren <ejscarengos@gmail.com>
2026-07-29 09:19:00 -0500

    Add files via upload

--- data.js
+++ data.js
@@ -1,75 +1,253 @@
-
-
 const profiles = [
   {
     id: 1,
-    name: 'temp name',
-    initials: 'SN',
-    location: 'California',
-    year: '2019',
-    summary: 'summary',
+    name: 'Johan Sebastián Duran Guerrero',
+    initials: '',
+    location: 'Biddeford, ME',
+    year: '2026',
+    summary: 'Cause of Death: Shot and killed by ICE agent at traffic stop',
     story: [
-      'story'
+      'Johan Sebastián Duran Guerrero was a person. He was described as a man full of dreams. His daughter was his entire world; he often slept beside her. His joy was infectious. Johan was killed Monday July 13, 2026.'
     ],
     pic: ''  
   },
   {
     id: 2,
-    name: 'temp name',
-    initials: 'SN',
-    location: 'Texas',
-    year: '2020',
-    summary: 'summary',
+    name: 'Lorenzo Salgado Araujo',
+    initials: '',
+    location: 'Houston, TX',
+    year: '2026',
+    summary: 'Cause of Death: Shot by ICE officer during enforcement operation',
     story: [
-      'story'
+      'Lorenzo Salgado Araujo was a person. He loved music and soccer. He always woke up early. He took the time to make electrolyte drinks for the men he worked with. He believed in the American Dream and came to the United States to give his children that dream. Lorenzo was killed Tuesday July 7, 2026.'
     ],
     pic: ''  
   },
   {
     id: 3,
-    name: 'temp name',
-    initials: 'SN',
-    location: 'Arizona',
-    year: '2021',
-    summary: 'summary',
+    name: 'Mamuka Artmeladze',
+    initials: '',
+    location: 'Winnifield, LA',
+    year: '',
+    summary: 'Cause of Death: Found unresponsive in custody',
     story: [
-      'story'
+      'Mamuka Artmeladze was a 43 year old from Georgia.'
     ],
     pic: ''  
   },
   {
     id: 4,
-    name: 'temp name',
-    initials: 'SN',
-    location: 'New York',
-    year: '2022',
-    summary: 'summary',
+    name: 'Alejandro Cabrera Clemente',
+    initials: '',
+    location: 'Winnifield, LA',
+    year: '',
+    summary: 'Cause of Death: Died after being rushed to the hospital - cause still under investigation',
     story: [
-      'story'
+      'Alejandro Cabrera Clemente was a 49 year old immigrant from Mexico.'
     ],
     pic: ''  
   },
   {
     id: 5,
-    name: 'temp name',
-    initials: 'SN',
-    location: 'Georgia',
-    year: '2023',
-    summary: 'summary',
+    name: 'Tuan Van Bui',
+    initials: '',
+    location: 'Bunker Hill, IN',
+    year: '',
+    summary: 'Cause of Death: Found unresponsive - cause currently still under investigation',
     story: [
-      'story'
+      'Tuan Van Bui was a 55 year old immigrant from Vietnam.'
     ],
     pic: ''  
-  }, 
+  },
   {
     id: 6,
-    name: 'temp name',
-    initials: 'SN',
-    location: 'Georgia',
-    year: '2027',
-    summary: 'summary',
+    name: 'Jose Guadalupe Ramos-Solano',
+    initials: '',
+    location: 'Adelanto, CA',
+    year: '',
+    summary: 'Cause of Death: Found unconscious and unresponsive - cause is still under investigation',
     story: [
-      'story'
+      'Jose Guadalupe Ramos-Solano was a 52 year old immigrant from Mexico.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 7,
+    name: 'Royer Perez-Jimenez',
+    initials: '',
+    location: 'Haven, FL',
+    year: '',
+    summary: 'Cause of Death: Presumed suicide - found unresponsive in cell',
+    story: [
+      'Royer Perez-Jimenez was a 19 year old immigrant from Mexico.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 8,
+    name: 'Mohommad Nazeer Paktyawal',
+    initials: '',
+    location: 'Dallas, TX',
+    year: '',
+    summary: 'Cause of Death: Died less than 24 hours after ICE detention - tongue swelling, cardiac event',
+    story: [
+      'Mohommad Nazeer Paktyawal was a 41 year old immigrant from Afghanistan.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 9,
+    name: 'Emmanuel Damas',
+    initials: '',
+    location: 'Scottsdale, AZ',
+    year: '',
+    summary: 'Cause of Death: Died after untreated dental infection spread - intubated on ventilator',
+    story: [
+      'Emmanuel Damas was an immigrant from Haiti.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 10,
+    name: 'Pejman Karshenas Najafabadi',
+    initials: '',
+    location: 'Natchez, MS',
+    year: '',
+    summary: 'Cause of Death: Cardiac arrest after chronic health conditions went undertreated in detention',
+    story: [
+      'Pejman Karshenas Najafabadi was a 59 year old immigrant from Iran.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 11,
+    name: 'Alberto Gutierrez-Reyes',
+    initials: '',
+    location: 'Adelanto, CA',
+    year: '',
+    summary: 'Cause of Death: Died after hospitalization for chest pain and shortness of breath - medical neglect alleged',
+    story: [
+      'Alberto Gutierrez-Reyes was a 48 year old immigrant from Mexico.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 12,
+    name: 'Jairo Garcia Hernandez',
+    initials: '',
+    location: 'Miami, FL',
+    year: '',
+    summary: 'Cause of Death: Collapsed unexpectedly - was immunocompromised and in ill health',
+    story: [
+      'Jairo Garcia Hernandez was a 27 year old immigrant from Guatemala.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 13,
+    name: 'Lorth Sim',
+    initials: '',
+    location: 'Hill, IN',
+    year: '',
+    summary: 'Cause of Death: Under investigation - found unresponsive in cell',
+    story: [
+      'Lorth Sim was a 59 year old immigrant from Cambodia.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 14,
+    name: 'Alex Pretti',
+    initials: '',
+    location: 'Minneapolis, MN',
+    year: '2026',
+    summary: 'Cause of Death: Shot and killed by Border Patrol agents during ICE operation',
+    story: [
+      'Alex Pretti was a person. As a kid he played basketball and track. His parents were the ones to drive the other kids around. They said he was always curious about the world around him; he was passionate about everything from the stars to his patients at the VA (Veterans Affairs). Alex was killed Saturday January 24, 2026.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 15,
+    name: 'Victor Manuel Diaz',
+    initials: '',
+    location: 'El Paso, TX',
+    year: '',
+    summary: 'Cause of Death: Presumed suicide - under investigation',
+    story: [
+      'Victor Manuel Diaz was a 36 year old immigrant from Nicaragua.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 16,
+    name: 'Heber Sanchez Dominguez',
+    initials: '',
+    location: 'Lovejoy, GA',
+    year: '',
+    summary: 'Cause of Death: Pending investigation',
+    story: [
+      'Heber Sanchez Dominguez was an immigrant from Mexico.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 17,
+    name: 'Parady La',
+    initials: '',
+    location: 'Philadelphia, PA',
+    year: '',
+    summary: 'Cause of Death: Severe drug withdrawal, anoxic brain injury, cardiac arrest, multiple organ failures',
+    story: [
+      'Parady La was a 46 year old immigrant from Cambodia.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 18,
+    name: 'Renee Nicole Good',
+    initials: '',
+    location: 'Minneapolis, MN',
+    year: '2026',
+    summary: 'Cause of Death: Shot by ICE agent during enforcement operation',
+    story: [
+      'Renee Good was a person. She was a mother of three. She was a wife, a sister, and a daughter. She believed, “We are here to love each other, care for each other, and keep each other safe.” Her wife described her as “pure sunshine”. Renee was killed Wednesday January 7, 2026.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 19,
+    name: 'Luis Beltran Yanez-Cruz',
+    initials: '',
+    location: 'Indio, CA',
+    year: '',
+    summary: 'Cause of Death: Heart-related health issues',
+    story: [
+      'Luis Beltran Yanez-Cruz was a 68 year old immigrant from Honduras.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 20,
+    name: 'Luis Gustavo Nunez Caceres',
+    initials: '',
+    location: 'Houston, TX',
+    year: '',
+    summary: 'Cause of Death: Heart-related issues',
+    story: [
+      'Luis Gustavo Nunez Caceres was a 42 year old immigrant from Honduras.'
+    ],
+    pic: ''  
+  },
+  {
+    id: 21,
+    name: 'Geraldo Lunas Campos',
+    initials: '',
+    location: 'Fort Bliss, TX',
+    year: '',
+    summary: 'Cause of Death: Homicide - choked to death by guards per medical examiner',
+    story: [
+      'Geraldo Lunas Campos was a 55 year old immigrant from Cuba.'
     ],
     pic: ''  
   }
